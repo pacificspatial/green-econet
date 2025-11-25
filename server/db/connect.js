e9c3f242c551bd8c0964836cb7869d1446a23306
Balachandran Viswanathan <tekgile@gmail.com>
2025-11-25 23:10:15 +0530

    db cleanup

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,6 +1,5 @@
 import { pool, sequelize } from "../config/dbConfig.js";
 import {
-  EnGreen,
   Projects,
   ProjectPolygons,
   ClippedBuffer125Green,
@@ -11,12 +10,6 @@ export const connectDB = async function () {
   try {
     await sequelize.authenticate();
     console.log("Database connection has been established successfully.");
-    //create tables if not created
-    await EnGreen.sync({ alter: true });
-    await Projects.sync({ alter: true });
-    await ProjectPolygons.sync({ alter: true });
-    await ClippedBuffer125Green.sync({ alter: true });
-    await ClippedGreen.sync({ alter: true });
   } catch (error) {
     console.error("Unable to connect to the database or sync models:", error);
   }
