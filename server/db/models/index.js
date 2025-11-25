a7ba17c5a38399fa9d00bfed26bf793acf3c2854
Balachandran Viswanathan <tekgile@gmail.com>
2025-11-25 23:14:41 +0530

    db and mock polling cleanup

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,10 @@
 
-import { EnGreen } from "./enGreen.js";
 import { Projects } from "./projects.js";
 import { ProjectPolygons } from "./projectPolygon.js";
 import { ClippedBuffer125Green } from "./clippedBuffer125Green.js";
 import { ClippedGreen } from "./clippedGreen.js";
 
 export {
-  EnGreen,
   Projects,
   ProjectPolygons,
   ClippedBuffer125Green,
