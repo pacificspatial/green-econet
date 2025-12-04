1b289130b1c94c786aee06e86da31356490f01b0
Balachandran Viswanathan <tekgile@gmail.com>
2025-12-04 13:09:57 +0530

    changes

--- server/config/server.js
+++ server/config/server.js
@@ -27,7 +27,7 @@ app.use(
       }
       return callback(new Error("Not allowed by CORS"));
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
